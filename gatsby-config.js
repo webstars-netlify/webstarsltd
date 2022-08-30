5533584db7930362f38e4a049b4034edc087cad9
buggatiews <jez+dev@webstarsltd.com>
2022-08-31 00:05:34 +0500

    gatsby-plugin-netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-fontawesome-css`,
+    `gatsby-plugin-netlify`,
     {
       resolve: "gatsby-source-storyblok",
       options: {
