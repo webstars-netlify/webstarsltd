e9c0280386d590a3de811544bdd7e3197fb6b41a
buggatiews <jez+dev@webstarsltd.com>
2022-08-31 00:58:11 +0500

    window undefined build issue

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,7 +4,6 @@ exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
 
   const projectTemplate = path.resolve("./src/templates/SingleProject.js")
-  const insightTemplate = path.resolve("./src/templates/SingleInsight.js")
 
   const res = await graphql(`
     query {
@@ -36,16 +35,6 @@ exports.createPages = async ({ graphql, actions }) => {
     createPage({
       component: projectTemplate,
       path: `/projects/${edge?.node?.slug}`,
-      context: {
-        slug: edge?.node?.slug,
-      },
-    })
-  })
-
-  insightQueryResponse?.data?.allStoryblokEntry.edges.forEach(edge => {
-    createPage({
-      component: insightTemplate,
-      path: `/insights/${edge?.node?.slug}`,
       context: {
         slug: edge?.node?.slug,
       },
