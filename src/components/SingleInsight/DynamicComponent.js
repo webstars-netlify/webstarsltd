4aba1fda13e1e2f0d38819ebccedb7ab785ef781
buggatie <m.mohsinbarkaati@gmail.com>
2022-09-04 00:53:59 +0500

    latest code

--- src/components/SingleInsight/DynamicComponent.js
+++ src/components/SingleInsight/DynamicComponent.js
@@ -3,7 +3,7 @@ import React from "react"
 // Page level import statements
 import Banner from "./Banner"
 import BannerImage from "./BannerImage"
-import InsightDescription from "./InsightDescription"
+// import InsightDescription from "./InsightDescription"
 import MarketingGuide from "./Guide"
 import Suggestion from "./Suggestion"
 import Text from "./Text"
@@ -11,7 +11,7 @@ import Text from "./Text"
 const Components = {
   teaser_text: Banner,
   insight_banner_img: BannerImage,
-  description_section: InsightDescription,
+  // description_section: InsightDescription,
   guide: MarketingGuide,
   bottom_text_copy: Text,
 }
