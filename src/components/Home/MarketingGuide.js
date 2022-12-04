39f0fbcdb80ab9b0989902e63743088ba9c6a969
bugattie <m.mohsinbarkaati@gmail.com>
2022-12-04 18:47:07 +0500

    bg color for marketing guide

--- src/components/Home/MarketingGuide.js
+++ src/components/Home/MarketingGuide.js
@@ -2,6 +2,8 @@ import React from "react"
 import { storyblokEditable } from "@storyblok/js"
 
 const MarketingGuide = ({ blok }) => {
+  console.log("Check --------")
+  console.log(blok.background_color_hex)
   return (
     <>
       <div {...storyblokEditable(blok)}>
