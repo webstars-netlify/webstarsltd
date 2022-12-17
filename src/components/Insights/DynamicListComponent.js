4c09e9c749c2d318509e515db7b7ad1303a31253
bugattie <m.mohsinbarkaati@gmail.com>
2022-12-17 22:22:21 +0500

    categorization fix

--- src/components/Insights/DynamicListComponent.js
+++ src/components/Insights/DynamicListComponent.js
@@ -10,7 +10,7 @@ const DynamicListComponent = ({ blok, slug, index, categoryTitle }) => {
   if (
     typeof Components[blok.component] !== "undefined" &&
     blok.component === "insight_display" &&
-    blok.insights[0].catgeory_name === "categoryTitle"
+    blok.insights[0].catgeory_name === categoryTitle
   ) {
     const Component = Components[blok.component]
     return <Component blok={blok} key={blok._uid} slug={slug} index={index} />
